a86022f4804fa92ea92d7f9d2e5081c0ff473dbd
michaelhodgejr <mikeh@lightsky.com>
2020-09-23 10:17:08 -0500

    Update user_agent_checker.rb

--- lib/rails_same_site_cookie/user_agent_checker.rb
+++ lib/rails_same_site_cookie/user_agent_checker.rb
@@ -39,11 +39,11 @@ module RailsSameSiteCookie
     end
 
     def is_ios_version?(major)
-      user_agent.os&.family == 'iOS' and user_agent.os&.version&.major == major
+      user_agent.os.family == 'iOS' and user_agent.os.version.major == major
     end
 
     def is_mac_osx_version?(major,minor)
-      user_agent.os&.family == 'Mac OS X' and user_agent.os&.version&.major == major and user_agent.os&.version&.minor == minor
+      user_agent.os.family == 'Mac OS X' and user_agent.os.version.major == major and user_agent.os.version.minor == minor
     end
 
     def is_safari?
@@ -74,14 +74,14 @@ module RailsSameSiteCookie
     end
 
     def is_uc_version_at_least?(major,minor,build)
-      if user_agent.version&.major&.to_i == major
-        if user_agent.version&.minor&.to_i == minor
-          return user_agent.version&.patch&.to_i >= build
+      if user_agent.version.major.to_i == major
+        if user_agent.version.minor.to_i == minor
+          return user_agent.version.patch.to_i >= build
         else
-          return user_agent.version&.minor&.to_i > minor
+          return user_agent.version.minor.to_i > minor
         end
       else
-        return user_agent.version&.major&.to_i > major
+        return user_agent.version.major.to_i > major
       end
     end
 
